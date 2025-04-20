fef5500f0b091de5c2017b07b6f1a2f22522fb38
ahmadhaytham <ahm672001@gmail.com>
2025-04-20 10:09:42 +0200

    Rest and JPA tasks

--- src/main/java/CourseApp/CourseApp/RatingsRecommender.java
+++ src/main/java/CourseApp/CourseApp/RatingsRecommender.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-@Primary
-@Component
-public class RatingsRecommender implements CourseRecommender{
-    @Override
-    public List<Course> recommendCourses() {
-        return List.of(new Course(1,"Angular","An Angular Course",4.7,350));
-    }
-}
+//@Primary
+//@Component
+//public class RatingsRecommender implements CourseRecommender{
+////    @Override
+////    public List<Course> recommendCourses() {
+////        return List.of(new Course(1,"Angular","An Angular Course",4.7,350));
+////    }
+//}
